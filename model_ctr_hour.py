eac64ba1cb7134784eb5add25283b5b73c5ae2f3
sangyongjia <sangyongjia@163.com>
2020-11-25 17:13:33 +0800

    1、使用全部特征（day+hour）， 2、dense特征按照dense的方式使用， 3、weight的特征按照 带权重的方式使用。

--- model_ctr_hour.py
+++ model_ctr_hour.py
@@ -80,23 +80,6 @@ if __name__ == "__main__":
                         fixed_feature_columns.append(DenseFeat(feat_name, 1, ))  # dense 特征
                     else:
                         continue
-
-            for feat_name in all_columns:
-                if feat_name[-6:] == 'weight' or feat_name in ['ctr_label', 'cvr_label']:
-                    select_columns_name.append(feat_name)
-                    continue
-                for key in vocabulary_size.keys():
-                    if key in feat_name:
-                        vocabulary_size_val = vocabulary_size[key]
-                        print("key:{0},size:{1},feature name:{2}".format(key, vocabulary_size_val, feat_name))
-                        break
-                print("size:{0},feature name:{1}".format(vocabulary_size_val, feat_name))
-                varlen_feature_columns.append(VarLenSparseFeat(
-                    SparseFeat(feat_name, vocabulary_size=vocabulary_size_val + 1, embedding_dim=4, use_hash=False),
-                    maxlen=1,
-                    combiner='mean', weight_name=feat_name + '_weight', weight_norm=False))
-                select_columns_name.append(feat_name)
-                vocabulary_size_val = 1
         # if use_hour_features:  # 复现最优结果
         #     for feat_name in all_columns:
         #         if feat_name[-6:] == 'weight' or feat_name in ['ctr_label', 'cvr_label']:
